d916fb544b0bf5d61a7c9acd2be16a8ab203fe79
jiwuchao <wuchaoji@pptv.com>
2017-12-05 14:32:45 +0800

    添加文件

---
+++ AlamofireTest/AlamofireTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AlamofireTest
+//
+//  Created by JiWuChao on 2017/12/5.
+//  Copyright © 2017年 JiWuChao. All rights reserved.
+//
+
+import UIKit
+
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+}
+
