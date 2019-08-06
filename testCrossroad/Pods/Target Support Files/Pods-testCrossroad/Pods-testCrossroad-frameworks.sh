330fc30e52b0a8017bcc3669178090ff2b1fafc3
jiwuchao <jiwuchao123@gmail.com>
2019-08-06 20:35:54 +0800

    add

--- testCrossroad/Pods/Target Support Files/Pods-testCrossroad/Pods-testCrossroad-frameworks.sh
+++ testCrossroad/Pods/Target Support Files/Pods-testCrossroad/Pods-testCrossroad-frameworks.sh
@@ -162,9 +162,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Crossroad/Crossroad.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/URLNavigator/URLNavigator.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Crossroad/Crossroad.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/URLNavigator/URLNavigator.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
